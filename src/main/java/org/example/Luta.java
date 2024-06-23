5e36ad586d28a16c76c9861944824f73fbc158e8
sergiolinss <sergiovictor2000@outlook.com>
2024-06-23 20:28:50 -0300

    Criação das lutas e testes

--- src/main/java/org/example/Luta.java
+++ src/main/java/org/example/Luta.java
@@ -1,15 +1,94 @@
 package org.example;
 
+import java.util.Random;
+
 public class Luta {
-    private String desafiado;
-    private String desafiante;
+    private Lutador desafiado;
+    private Lutador desafiante;
     private int rounds;
     private boolean aprovada;
 
-    public void marcarLuta(){
+    public void marcarLuta(Lutador l1, Lutador l2) {
+        if (l1.getCategoria() == l2.getCategoria() && l1 != l2) {
+            setAprovada(true);
+            desafiante = l1;
+            desafiado = l2;
+            System.out.println(" Luta aprovada ");
+        } else {
+            setAprovada(false);
+            this.desafiado = null;
+            this.desafiante = null;
+            System.out.println(" Luta não foi aprovada ");
+        }
 
     }
-    public void lutar(){
 
+    public void lutar() {
+        if (getAprovada()) {
+            System.out.println("### Desafiado ###");
+            this.desafiado.apresentar();
+            System.out.println("### Desafiante ###");
+            this.desafiante.apresentar();
+
+            Random random = new Random();
+            int vencedor = random.nextInt(3);
+
+            switch (vencedor) {
+                case 0: // empate
+                    System.out.println(" A luta empatou");
+                    this.desafiado.empatarLuta();
+                    this.desafiante.empatarLuta();
+                    break;
+
+                case 1: // Ganhou desafiado
+                    System.out.println(this.desafiado.getNome() + " Venceu a luta ");
+                    this.desafiado.ganharLuta();
+                    this.desafiante.perderLuta();
+                    break;
+
+                case 2: // Ganhou desafiante
+                    System.out.println(this.desafiante.getNome() + " Venceu a luta ");
+                    this.desafiante.ganharLuta();
+                    this.desafiado.perderLuta();
+                    break;
+            }
+
+
+        } else {
+            System.out.println(" A luta não pode acontecer");
+        }
+
+    }
+
+    public boolean getAprovada() {
+        return aprovada;
+    }
+
+    public void setAprovada(boolean aprovada) {
+        this.aprovada = aprovada;
+    }
+
+    public int getRounds() {
+        return rounds;
+    }
+
+    public void setRounds(int rounds) {
+        this.rounds = rounds;
+    }
+
+    public Lutador getDesafiante() {
+        return desafiante;
+    }
+
+    public void setDesafiante(Lutador desafiante) {
+        this.desafiante = desafiante;
+    }
+
+    public Lutador getDesafiado() {
+        return desafiado;
+    }
+
+    public void setDesafiado(Lutador desafiado) {
+        this.desafiado = desafiado;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,8 +11,16 @@ public class Main {
    lutadores[3] = new Lutador("Dead Code", "Australia", 28, 1.93f, 81.6f, 13, 0, 2);
    lutadores[4] = new Lutador("Ufucobol", "Brasil", 37, 1.70f, 119.3f, 5, 4, 3);
    lutadores[5] = new Lutador("Nerdard", "Eua", 30, 1.81f, 105.7f, 12, 2, 4);
-   lutadores[0].apresentar();
-   lutadores[0].Status();
+
+
+
+   Luta UEC01 = new Luta();
+   UEC01.marcarLuta(lutadores[0], lutadores[0]);
+   UEC01.lutar();
+  // lutadores[0].Status();
+   //lutadores[1].Status();
+
+
 
     }
 }
